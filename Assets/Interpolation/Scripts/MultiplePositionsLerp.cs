904c4156839d238041b046c723e15167b0ad1805
matt-merritt-phs <matthewmerritt@pallottihs.org>
2026-04-23 11:40:19 -0400

    finished lerp between multiple different locations

--- Assets/Interpolation/Scripts/MultiplePositionsLerp.cs
+++ Assets/Interpolation/Scripts/MultiplePositionsLerp.cs
@@ -8,9 +8,9 @@ public class MultiplePositionsLerp : MonoBehaviour
     public int targetLocationIndex;
     public float movementDuration;
 
-    public Vector3 moveStartLocation;
-    public bool isMoving;
-    public float currentTimeMoving;
+    private Vector3 moveStartLocation;
+    private bool isMoving;
+    private float currentTimeMoving;
 
     void Start()
     {
@@ -22,35 +22,37 @@ public class MultiplePositionsLerp : MonoBehaviour
         // start movement when space is pressed
         if (Input.GetKeyDown(KeyCode.Alpha1) && targetLocationIndex != 0)
         {
+            moveStartLocation = transform.position;
             targetLocationIndex = 0;
-            currentTimeMoving -= movementDuration;
+            currentTimeMoving = 0;
             isMoving = true;
         }
         if (Input.GetKeyDown(KeyCode.Alpha2) && targetLocationIndex != 1)
         {
+            moveStartLocation = transform.position;
             targetLocationIndex = 1;
-            currentTimeMoving -= movementDuration;
+            currentTimeMoving = 0;
             isMoving = true;
         }
         if (Input.GetKeyDown(KeyCode.Alpha3) && targetLocationIndex != 2)
         {
+            moveStartLocation = transform.position;
             targetLocationIndex = 2;
-            currentTimeMoving -= movementDuration;
+            currentTimeMoving = 0;
             isMoving = true;
         }
         if (Input.GetKeyDown(KeyCode.Alpha4) && targetLocationIndex != 3)
         {
+            moveStartLocation = transform.position;
             targetLocationIndex = 3;
-            currentTimeMoving -= movementDuration;
+            currentTimeMoving = 0;
             isMoving = true;
         }
 
         // mark as not moving once target is reached
         if (currentTimeMoving > movementDuration)
         {
-            currentTimeMoving = movementDuration; // adjust value to not go over
             isMoving = false;
-            moveStartLocation = transform.position;
         }
 
         // count how long we have been moving
